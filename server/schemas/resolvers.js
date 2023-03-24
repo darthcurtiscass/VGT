27f56aa9e66a0570b6641b351ceaa15479b4c3cf
darthcurtiscass <114704942+darthcurtiscass@users.noreply.github.com>
2023-03-23 18:53:20 -0600

    created file structure

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,10 @@
+const { AuthenticationError } = require('apollo-server-express');
+const { User, } = require('../models');
+const { signToken } = require('../utils/auth');
+// const stripe = require('stripe')('sk_test_4eC39HqLyjWDarjtT1zdp7dc');
+
+const resolvers = {
+  
+};
+
+module.exports = resolvers;
