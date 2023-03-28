c72640f9ca164e8509aa8a6152ab0fe58b536ce1
darthcurtiscass <114704942+darthcurtiscass@users.noreply.github.com>
2023-03-27 20:50:35 -0600

    added resolvers with Max

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -22,7 +22,85 @@ const resolvers = {
       quizes: async () => {
         return Quiz.find().populate('scores');
       }
+  },
+  Mutation: {
+    addUser: async (parent, {username, email, password}) => {
+      const user = await User.create({ username, email, password});
+      const token = signToken(user);
+      return { token, user }
+    },
+    login: async (parent, { email, password }) => {
+      const user = await User.findOne({ email });
 
+      if (!user) {
+        throw new AuthenticationError("Email or password incorrect");
+      }
+
+      const correctPw = await user.isCorrectPassword(password);
+
+      if (!correctPw) {
+        throw new AuthenticationError("Incorrect credentials");
+      }
+
+      const token = signToken(user);
+
+      return { token, user };
+    },
+    addScore: async (parent, { score }, context) => {
+      try {
+        if(context.user) { 
+          return User.findOneAndUpdate(
+            { username: context.user.username },
+            {
+              $addToSet: {
+                scores: { score }
+              }
+            }
+          )
+        }
+      } catch(err) {
+        console.log(err)
+        throw new AuthenticationError("couldn't save your score. You know nothing.")
+      }
+    },
+    addFriend: async (parent, { friendId }, context) => {
+      try {
+        if(context.user) {
+          return User.findOneAndUpdate(
+            { _id: context.user._id },
+            {
+              $addToSet: {
+                friends: { friendId }
+              }
+            },
+            {
+              new: true,
+              validators: true
+            }
+          )
+        }
+      } catch(err) {
+        console.log(err)
+        throw new AuthenticationError("nobody wants to be your friend")
+      }
+    },
+    removeFriend: async (parent, { friendId }, context) => {
+      try {
+        if(context.user) {
+          return User.findOneAndUpdate(
+            { _id: context.user._id },
+            {
+              $pull: {
+                friends: { friendId }
+              },
+            },
+          )
+        }
+      } catch(err) {
+        console.log(err)
+        throw new AuthenticationError("you have a clinger")
+      }
+    }
   }
 };
 
