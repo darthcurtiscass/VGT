2b49dc9f16e9b9fa2a51a4a6f9f766317cb70900
Christian Loera <114829265+ArticFox04@users.noreply.github.com>
2023-03-28 21:30:05 -0600

    added profile functions and added profile route

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { creaRoot } from 'react-dom/client';
 import './index.css';
 import App from './App';
 import 'antd/dist/reset.css';
