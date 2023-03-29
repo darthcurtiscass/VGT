a02092225fa7f2f81f87cc9b398bd7b7d3a99c53
tteklu <teamirteklu@gmail.com>
2023-03-28 20:03:45 -0600

    added navbar functionality + login and signup pages

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,23 +1,29 @@
-import React from 'react';
+import { React } from 'react';
+import { Route, NavLink, Link, Routes } from 'react-router-dom';
 import '../../src/App.css';
-import { Breadcrumb, Layout, Menu, theme, xs } from 'antd';
+import { Layout, Menu, theme } from 'antd';
+import Home from '../pages/Home';
+import Leaderboard from '../pages/Leaderboard';
+import Questionnaire from '../pages/Questionnaire';
+import Signup from '../pages/Signup';
+import Login from '../pages/Login';
 
 
- function Header() {
-    return(
-      <div>
-      <header style={{position:'sticky', top: 0, zIndex: 1, width: '100%'}} className="App-header">
+function Header() {
+  return (
+    <div>
+      <header style={{ position: 'sticky', top: 0, zIndex: 1, width: '100%' }} className="App-header">
 
         <div style={{
-            float: 'left',
-            width: 120,
-            height: 31,
-            margin: '8px 8px 8px 8px',
-            padding: '8px 0px 8px 0px',
-            background: 'gold',
-            fontFamily: '',
-          }}>
-            <h1>VGT</h1>
+          float: 'left',
+          width: 120,
+          height: 31,
+          margin: '8px 8px 8px 8px',
+          padding: '8px 0px 8px 0px',
+          background: 'gold',
+          fontFamily: '',
+        }}>
+          <h1>VGT</h1>
         </div>
 
         {/* <nav mode="horizontal" theme="dark" className="main-header-menu">
@@ -40,40 +46,44 @@ import { Breadcrumb, Layout, Menu, theme, xs } from 'antd';
           </section>
         </nav> */}
 
-        <Menu 
-        mode='horizontal'
-        style={{
-            backgroundColor:'gold',
-        }}
-        
-        items={[
-            {
-                label: 'Home',
-                key: 'Home',
-            },
-            {
-                label: 'Leaderboard',
-                key: 'Leaderboard',
-            },
-            {
-                label: 'Quiz',
-                key: 'Quiz',
-            },
-            {
-                label: 'Signup',
-                key: 'Signup',
-            },
-            {
-                label: 'Login',
-                key: 'Login',
-            },
-            ]}>
+        <Menu
+          mode='horizontal'
+          style={{
+            backgroundColor: 'gold',
+          }}>
 
+          <Menu.Item key="home">
+            <NavLink to="/">Home</NavLink>
+          </Menu.Item>
+
+          <Menu.Item key="leaderboard">
+            <NavLink to="/leaderboard">Leaderboard</NavLink>
+          </Menu.Item>
+
+          <Menu.Item key="quiz">
+            <NavLink to="/quiz">Quiz</NavLink>
+          </Menu.Item>
+
+          <Menu.Item key="signup" element={<Signup />}>
+            <NavLink to="/signup">Signup</NavLink>
+          </Menu.Item>
+
+          <Menu.Item key="login">
+            <NavLink to="/login">Login</NavLink>
+          </Menu.Item>
         </Menu>
-        
+
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/leaderboard" element={<Leaderboard />} />
+          <Route exact path="/quiz" element={<Questionnaire />} />
+          <Route exact path="/signup" element={<Signup />} />
+          <Route exact path="/login" element={<Login />} />
+        </Routes>
+
       </header>
-     </div>
-    );
+    </div>
+  );
 }
 
 
