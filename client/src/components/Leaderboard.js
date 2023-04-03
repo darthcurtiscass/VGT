f08e9011beb28bd26666d4edc183b8df6d146c71
Max Tribble <maxtribble@live.com>
2023-04-03 04:52:19 -0600

    added basic leaderboard function - leaderboard component and leaderboard page that displays users

--- client/src/components/Leaderboard.js
+++ client/src/components/Leaderboard.js
@@ -3,4 +3,25 @@ import '../index.css';
 import { Pagination } from 'antd';
 const Leaderboard = () => <Pagination defaultCurrent={1} total={50}/>
 
-export default Leaderboard;
+
+//use state variable for i, instead of defining it as a specific index number.
+
+const Scores = ({ users }) => {
+  return (
+    <div>
+
+          <div className="card mb-3">
+            <h4 className="card-header bg-primary text-light p-2 m-0 bg-dark">
+            </h4>
+            {users.users.map((user) => (
+                <div style={{border: "1px solid black"}}>
+                    {user.username}{user.score}
+                </div>
+            ))}
+          </div>
+
+    </div>
+  );
+};
+
+export default Scores;
